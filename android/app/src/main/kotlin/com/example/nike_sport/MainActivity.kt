0cc6748c284e81732b68e028cd92134fc65583cb
wijangggggg <wijangsriyadi@gmail.com>
2025-06-12 12:09:48 +0700

    memulai projek

---
+++ android/app/src/main/kotlin/com/example/nike_sport/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.nike_sport
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/startscreen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class StartPage extends StatelessWidget {
+  const StartPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+ 
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/home');
+                  },
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.symmetric(vertical: 16),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                  child: Text(
+                    'START',
+                    style: TextStyle(
+                      fontSize: 18,
+                      letterSpacing: 1,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
